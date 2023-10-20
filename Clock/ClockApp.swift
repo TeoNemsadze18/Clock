c4826b5218efe7a9a2c6d0ded215451822efa27b
TeoNemsadze <teona.nemsadze.2@iliauni.edu.ge>
2023-10-20 12:29:12 +0400

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by teona nemsadze on 20.10.23.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
